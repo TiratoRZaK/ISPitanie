5b82c62682d58fdb1de82d30917cb6d2a29b98fc
Andrej Petrov <apetrov-radnaadr@yandex.ru>
2019-06-04 22:57:46 +0400

    Add dedicated class for AutoMapper initialization routine

--- ISPitanie.Application/App.xaml.cs
+++ ISPitanie.Application/App.xaml.cs
@@ -1,12 +1,4 @@
 ﻿using System.Windows;
-using AutoMapper;
-using DAL.DTO;
-using DAL.Interfaces;
-using DAL.Repositories;
-using ISPitanie.BLL.Entities;
-using ISPitanie.Interfaces;
-using ISPitanie.Models;
-using ISPitanie.Services;
 
 namespace ISPitanie
 {
@@ -17,18 +9,7 @@ namespace ISPitanie
     {
         public App()
         {
-            Mapper.Initialize(cfg =>
-            {
-                cfg.CreateMap<DishDTO, Dish>();
-                cfg.CreateMap<ProductDTO, Product>()
-                    .ForMember(x => x.UnitName, opt => opt.MapFrom(c => c.Unit.Name));
-                cfg.CreateMap<ProductDishDTO, ProductDish>();
-                cfg.CreateMap<UnitDTO, Unit>();
-                cfg.CreateMap<ProductViewModel, Product>()
-                    .ForMember(x => x.ProductsDishes, opt => opt.Ignore());
-            });
-
-
+            AutoMapperConfig.Initialize();
         }
     }
 }
---
+++ ISPitanie.Application/AutoMapperConfig.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using DAL.DTO;
+using ISPitanie.BLL.Entities;
+using ISPitanie.Models;
+
+namespace ISPitanie
+{
+    public static class AutoMapperConfig
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize(AddMappings);
+        }
+
+        private static void AddMappings(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<DishDTO, Dish>();
+            cfg.CreateMap<ProductDTO, Product>()
+                .ForMember(x => x.UnitName, opt => opt.MapFrom(c => c.Unit.Name));
+            cfg.CreateMap<ProductDishDTO, ProductDish>();
+            cfg.CreateMap<UnitDTO, Unit>();
+
+            cfg.CreateMap<ProductViewModel, Product>()
+                .ForMember(x => x.ProductsDishes, opt => opt.Ignore());
+
+            cfg.CreateMap<Product, ProductViewModel>();
+        }
+    }
+}
